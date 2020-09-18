4f1be27a9cb6772eb840e4cc318e20c66011391c
surerever <nike-gri123@mail.ru>
2020-09-18 06:46:42 +0300

    Updates

---
+++ precache-manifest.fcbe56de44f2411b58a2df6ed99f42ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2aa0fc517e8a87e0d432ebbee9b8408",
+    "url": "/vezdekod/index.html"
+  },
+  {
+    "revision": "aea51cad0d7cdd690c59",
+    "url": "/vezdekod/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "2498553e8c5da86fb6fd",
+    "url": "/vezdekod/static/css/main.e3730f79.chunk.css"
+  },
+  {
+    "revision": "aea51cad0d7cdd690c59",
+    "url": "/vezdekod/static/js/2.d72f8429.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/vezdekod/static/js/2.d72f8429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2498553e8c5da86fb6fd",
+    "url": "/vezdekod/static/js/main.c3026dbf.chunk.js"
+  },
+  {
+    "revision": "1e759f8b2be4d1c777ce",
+    "url": "/vezdekod/static/js/runtime-main.df521c9a.js"
+  }
+]);
